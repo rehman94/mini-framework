d594c75e6022969bed3b6a3f124da05f13ddd09f
Abdul Rehman <Abdul Rehman>
2016-12-03 21:58:07 +0500

    first commit

--- app/routes.php
+++ app/routes.php
@@ -1,8 +1,8 @@
 <?php
 /* lets create routes */
-$router->get('php-practicioner/pdo-refactoring', 'Pagescontroller@index');
-$router->get('php-practicioner/pdo-refactoring/index.php', 'Pagescontroller@index');
-$router->get('php-practicioner/pdo-refactoring/index.php/about', 'Pagescontroller@about');
-$router->get('php-practicioner/pdo-refactoring/index.php/contact', 'Pagescontroller@contact');
-$router->get('php-practicioner/pdo-refactoring/index.php/users', 'UsersController@index');
-$router->post('php-practicioner/pdo-refactoring/index.php/user', 'UsersController@store');
+$router->get('', 'Pagescontroller@index');
+$router->get('index.php', 'Pagescontroller@index');
+$router->get('index.php/about', 'Pagescontroller@about');
+$router->get('index.php/contact', 'Pagescontroller@contact');
+$router->get('index.php/users', 'UsersController@index');
+$router->post('index.php/user', 'UsersController@store');
